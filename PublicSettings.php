567bbcfb83ec1b442148285101849a657e4b5a5e
Lethosor <lethosor@users.noreply.github.com>
2014-12-08 21:42:43 -0500

    Create PublicSettings.php

---
+++ PublicSettings.php
@@ -0,0 +1,6 @@
+<?php
+#####################################################################################
+# This file stores public configuration settings for the DF wiki.
+# Note that changes made in this file are not automatically deployed to the wiki.
+#####################################################################################
+
